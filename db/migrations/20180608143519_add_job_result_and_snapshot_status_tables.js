f0244720daf52806d69545d6637b3fcd83e02725
Spencer Dangel <dangelspencer@gmail.com>
2018-06-08 14:52:57 -0400

    REP-83 Add knex migration for job_result and snapshot_status tables

---
+++ db/migrations/20180608143519_add_job_result_and_snapshot_status_tables.js
@@ -0,0 +1,57 @@
+
+exports.up = async function (knex, Promise) {
+    await knex.transaction(async (tx) => {
+        try {
+            // create tables
+            await tx.schema.createTable('job_result', t => {
+                t.integer('id')
+                    .notNullable()
+                    .primary();
+                t.text('name');
+            });
+
+            await tx.schema.createTable('snapshot_status', t => {
+                t.integer('id')
+                    .notNullable()
+                    .primary();
+                t.text('name');
+            });
+
+            await tx.schema.raw(`INSERT INTO job_result (id, name) VALUES
+                (0, 'default'),
+                (1, 'started'),
+                (2, 'success'),
+                (3, 'failed');`);
+
+            await tx.schema.raw(`INSERT INTO snapshot_status (id, name) VALUES
+                (0, 'default'),
+                (1, 'created'),
+                (2, 'deleted'),
+                (3, 'destroy_failed'),
+                (4, 'receive_failed'),
+                (5, 'deleting');`);
+
+            await tx.commit();
+        } catch (err) {
+            console.log(err);
+            await tx.rollback();
+        }
+    });
+};
+
+exports.down = async function (knex, Promise) {
+    await knex.transaction(async (tx) => {
+        try {
+            await tx.schema.dropTable('job_result');
+
+            await tx.schema.dropTable('snapshot_status');
+
+            await tx.commit();
+        } catch (err) {
+            console.log(err);
+            await tx.rollback();
+        }
+    });
+};
+
+exports.config = { transaction: false };
